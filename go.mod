e8a8dda32673176620d26bf8579e46092dccbd9a
Elliot Courant <me@elliotcourant.dev>
2019-11-18 22:12:53 -0600

    Building out basic interfaces around models.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/elliotcourant/mellivora
 go 1.12
 
 require (
+	github.com/ahmetb/go-linq/v3 v3.1.0
 	github.com/elliotcourant/meles v0.0.0-20190910230714-fdd8f30b2883
 	github.com/stretchr/testify v1.4.0
 )
--- model.go
+++ model.go
@@ -2,20 +2,95 @@ package mellivora
 
 import (
 	"fmt"
+	"github.com/ahmetb/go-linq/v3"
 	"reflect"
 )
 
-type ModelInfo struct {
-	ModelId uint32
-	Name    string
+var (
+	_ Model    = &modelInfo{}
+	_ Field    = &modelField{}
+	_ FieldSet = &fieldSet{}
+)
+
+type Model interface {
+	ModelId() uint32
+	Name() string
+	Fields() FieldSet
+	PrimaryKey() FieldSet
 }
 
-type ModelField struct {
-	model *ModelInfo
+type Field interface {
+	FieldId() uint32
+	Name() string
+}
 
-	FieldId uint32
-	Name    string
-	Reflect reflect.StructField
+type FieldSet interface {
+	GetAll() []Field
+	GetById(fieldId uint32) Field
+	GetByName(fieldName string) Field
+}
+
+type modelInfo struct {
+	modelId    uint32
+	name       string
+	fields     FieldSet
+	primaryKey FieldSet
+}
+
+func (m *modelInfo) ModelId() uint32 {
+	return m.modelId
+}
+
+func (m *modelInfo) Name() string {
+	return m.name
+}
+
+func (m *modelInfo) Fields() FieldSet {
+	return m.fields
+}
+
+func (m *modelInfo) PrimaryKey() FieldSet {
+	return m.primaryKey
+}
+
+type modelField struct {
+	model *modelInfo
+
+	fieldId    uint32
+	name       string
+	reflection reflect.StructField
+}
+
+func (m *modelField) FieldId() uint32 {
+	return m.fieldId
+}
+
+func (m *modelField) Name() string {
+	return m.name
+}
+
+type fieldSet struct {
+	model *modelInfo
+
+	fields []Field
+}
+
+func (f *fieldSet) GetById(fieldId uint32) Field {
+	return linq.From(f.fields).FirstWith(func(i interface{}) bool {
+		field, ok := i.(Field)
+		return ok && field.FieldId() == fieldId
+	}).(Field)
+}
+
+func (f *fieldSet) GetByName(fieldName string) Field {
+	return linq.From(f.fields).FirstWith(func(i interface{}) bool {
+		field, ok := i.(Field)
+		return ok && field.Name() == fieldName
+	}).(Field)
+}
+
+func (f *fieldSet) GetAll() []Field {
+	return f.fields
 }
 
 func getBaseTypeOf(model interface{}) reflect.Type {
@@ -34,6 +109,6 @@ func getBaseTypeOf(model interface{}) reflect.Type {
 	}
 }
 
-// func getModelInfo(model interface{}) ModelInfo {
+// func getModelInfo(model interface{}) modelInfo {
 // 	typ := getBaseTypeOf(model)
 // }
